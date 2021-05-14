7bc533329fea5e11a4441aa60a0b48fdf7d08823
Denis Kalinichenko <denis@kalinichenko.me>
2021-05-14 04:25:59 +0200

    [fix] removed useCallback

--- applications/client/src/state/Provider.tsx
+++ applications/client/src/state/Provider.tsx
@@ -1,4 +1,4 @@
-import React, {createContext, useCallback, ReactElement} from "react";
+import React, {createContext, useReducer, ReactElement} from "react";
 import rootReducer from "./reducers/root.reducer";
 import {IApplicationState} from "./types";
 import {initialState} from "./initialState";
@@ -17,11 +17,9 @@ export const GlobalStore = createContext({} as IStateContext);
 const asyncer = (dispatch: any, state: IApplicationState) => (action: any) =>
     typeof action === 'function' ? action(dispatch, state) : dispatch(action);
 
-
 export function StateProvider(props: StateProviderProps) {
-    const [state, dispatchBase] = React.useReducer(rootReducer, initialState);
-
-    const dispatch = useCallback(asyncer(dispatchBase, state), [])
+    const [state, dispatchBase] = useReducer(rootReducer, initialState);
+    const dispatch = asyncer(dispatchBase, state);
 
     return (
         <GlobalStore.Provider value={{ state, dispatch }}>
