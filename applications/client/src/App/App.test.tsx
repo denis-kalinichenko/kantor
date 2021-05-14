025d385ef88ffe4cc8fdbd15dca828ca7fd2d776
Denis Kalinichenko <denis@kalinichenko.me>
2021-05-14 15:07:25 +0200

    [fix] test

--- applications/client/src/App/App.test.tsx
+++ applications/client/src/App/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import {App} from './';
+import {StateProvider} from "../state/Provider";
+import {BrowserRouter as Router} from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders app without exploding', () => {
+  const app = render(
+      <StateProvider>
+        <Router>
+          <App />
+        </Router>
+      </StateProvider>
+  );
+  expect(app).toBeDefined();
 });
