57c29285c21022ae700877daf08dc46243462592
Denis Kalinichenko <denis@kalinichenko.me>
2021-05-17 01:47:56 +0200

    [feat] coverage report gh badges

--- packages/react-currency-exchange-widget/jest.config.js
+++ packages/react-currency-exchange-widget/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     roots: ['<rootDir>/src'],
     collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts', '!src/types/*', '!src/**/*.styled.tsx'],
     coverageDirectory: "../../applications/client/public/coverage",
+    coverageReporters: ['json-summary', 'text', 'lcov'],
     testRunner: require.resolve('jest-circus/runner'),
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
@@ -17,5 +18,5 @@ module.exports = {
     testEnvironment: 'jsdom',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     resetMocks: true,
-    verbose: true
+    verbose: true,
 }
